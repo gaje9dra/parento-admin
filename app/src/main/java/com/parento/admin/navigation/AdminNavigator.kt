af2df684a2a3689888e30651017dafb25b43d295
gaje9dra <126646046+gaje9dra@users.noreply.github.com>
2026-09-25 16:29:31 +0530

    feat(admin): preserve selected device in navigation

--- app/src/main/java/com/parento/admin/navigation/AdminNavigator.kt
+++ app/src/main/java/com/parento/admin/navigation/AdminNavigator.kt
@@ -6,7 +6,23 @@ class AdminNavigator(
     var currentDestination: AdminDestination = initialDestination
         private set
 
+    var selectedDeviceId: String? = null
+        private set
+
     fun navigate(destination: AdminDestination) {
         currentDestination = destination
+        if (destination != AdminDestination.DEVICE_DETAIL) {
+            selectedDeviceId = null
+        }
+    }
+
+    fun navigateToDevice(deviceId: String) {
+        selectedDeviceId = deviceId
+        currentDestination = AdminDestination.DEVICE_DETAIL
+    }
+
+    fun backToDevices() {
+        selectedDeviceId = null
+        currentDestination = AdminDestination.DEVICES
     }
 }

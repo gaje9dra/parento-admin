125c23cda332cae7fe86cf5eb43dce300992bd70
gaje9dra <126646046+gaje9dra@users.noreply.github.com>
2026-09-26 13:04:51 +0530

    fix: close LocationScreen container in state renderer

--- app/src/main/java/com/parento/admin/MainActivity.kt
+++ app/src/main/java/com/parento/admin/MainActivity.kt
@@ -408,7 +408,7 @@ class MainActivity : AppCompatActivity() {
                                     renderAuthenticatedState()
                                 },
                             ).render(state)
-                        }
+                        })
                     }
                 }
             }
